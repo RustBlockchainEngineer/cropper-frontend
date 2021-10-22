31325d569425dc12b48f7f738b4e2920d2f7784e
elmosqui <88122276+elmosqui@users.noreply.github.com>
2021-10-22 13:41:01 +0200

    test RPC

--- src/plugins/web3.ts
+++ src/plugins/web3.ts
@@ -49,7 +49,7 @@ const web3Plugin: Plugin = async (ctx, inject) => {
   if(DEVNET_MODE)
     endpoint = 'https://api.devnet.solana.com'; 
   else
-   endpoint = 'https://cropper.genesysgo.net';
+   endpoint = 'https://lokidfxnwlabdq.main.genesysgo.net:8899';
   
   const web3 = createWeb3Instance(endpoint)
 
