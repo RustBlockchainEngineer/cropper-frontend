8ed8a8bc717228d4d9d52b7b7c1a7f3afabf8343
elmosqui <88122276+elmosqui@users.noreply.github.com>
2021-10-25 21:37:29 +0200

    Update project.vue

--- src/pages/fertilizer/project.vue
+++ src/pages/fertilizer/project.vue
@@ -1138,11 +1138,11 @@ export default Vue.extend({
             const liquidityItem = get(this.liquidity.infos, lp.mintAddress)
 
             if (this.labelizedAmms[newFarmInfo.poolId]) {
-          console.log('ic0', lp, this.labelizedAmmsExtended[newFarmInfo.poolId]?.pfo, newFarmInfo.poolId, this.labelizedAmmsExtended[newFarmInfo.poolId].pfarmID);
+          console.log('ic0', lp, this.labelizedAmmsExtended[newFarmInfo.poolId]?.pfo, newFarmInfo.poolId, this.labelizedAmmsExtended[newFarmInfo.poolId]?.pfarmID);
               labelized = true
               if (
                 this.labelizedAmmsExtended[newFarmInfo.poolId]?.pfo == true &&
-                newFarmInfo.poolId == this.labelizedAmmsExtended[newFarmInfo.poolId].pfarmID
+                newFarmInfo.poolId == this.labelizedAmmsExtended[newFarmInfo.poolId]?.pfarmID
               ) {
           console.log('ic01', lp);
                 isPFO = true
