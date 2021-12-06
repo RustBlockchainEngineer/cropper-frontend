41959f209dcbbe0e83f9fb3a5f40150770f4c099
elmosqui <88122276+elmosqui@users.noreply.github.com>
2021-12-06 17:11:57 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -85,27 +85,27 @@ export default {
       display: "standalone",
       icons: [
         {
-          src: "favicon.ico",
+          src: "/favicon.ico",
           sizes: "64x64 32x32 24x24 16x16",
           type: "image/x-icon"
         },
         {
-          src: "apple-icon-72x72.png",
+          src: "/apple-icon-72x72.png",
           sizes: "72x72",
           type: "image/png"
         },
         {
-          src: "apple-icon-152x152.png",
+          src: "/apple-icon-152x152.png",
           sizes: "152x152",
           type: "image/png"
         },
         {
-          src: "logo192.png",
+          src: "/logo192.png",
           type: "image/png",
           sizes: "192x192"
         },
         {
-          src: "logo512.png",
+          src: "/logo512.png",
           type: "image/png",
           sizes: "512x512"
         }
