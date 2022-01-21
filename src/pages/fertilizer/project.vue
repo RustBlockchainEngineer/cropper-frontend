55801db614144afe0888d1623a688018ad377cec
elmosqui <88122276+elmosqui@users.noreply.github.com>
2022-01-21 14:47:50 +0100

    Update project.vue

--- src/pages/fertilizer/project.vue
+++ src/pages/fertilizer/project.vue
@@ -825,6 +825,7 @@ export default Vue.extend({
 
   data() {
     return {
+      TEST_TIME: 1643500800000,
       fertilizer: {
         picture: '/fertilizer/banner/unq.png',
         logo: '/fertilizer/logo/unq.png',
@@ -864,7 +865,7 @@ export default Vue.extend({
         sales_start_date: TEST_TIME + 60000 * 15,
         sales_end_date: TEST_TIME + 60000 * 20,
         distribution_start_date: TEST_TIME + 60000 * 25
-      },
+      } ,
       projectStatus: {
         preparation: 'Preparation',
         whitelist: 'Whitelist Open',
@@ -906,6 +907,56 @@ export default Vue.extend({
     // this.$router.push({ path: `/swap/` })
     this.currentTimestamp = moment().valueOf()
     this.setTimer()
+
+
+
+    const query = new URLSearchParams(window.location.search)
+    if (query.get('tt')) this.TEST_TIME = (query.get('tt') as any) * 1
+
+
+    this.fertilizer = {
+        picture: '/fertilizer/banner/unq.png',
+        logo: '/fertilizer/logo/unq.png',
+        title: 'UNQ.club',
+        short_desc: 'Social platform for NFT asset management',
+        long_desc:
+          'Whether a professional collector or aspiring enthusiast - UNQ is a place where you can take your game to the next level.',
+        hard_cap: '3000K',
+        pool_size: 5000,
+        participants: 100418,
+        website: 'UNQ.club',
+        website_url: 'https://UNQ.club',
+        mint: 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
+        ido_info: {
+          hard_cap: 140000,
+          sale_rate: 0.028,
+          sale_type: 'Vested',
+          open_time: 1643500800000,
+          close_time: 1643500800000
+        },
+        token_info: {
+          symbol: 'UNQ',
+          category: 'NFT',
+          token_distribution: 1643500800000,
+          blockchain: 'Solana'
+        },
+        img: {
+          about: '/fertilizer/project/unq/about.png',
+          features: '/fertilizer/project/unq/features.png',
+          roadmap: '/fertilizer/project/unq/roadmap.png',
+          team: '/fertilizer/project/unq/team.png',
+          tokenomics: '/fertilizer/project/unq/tokenomics.png',
+          distribution: '/fertilizer/project/unq/distribution.png'
+        },
+        whitelist_start_date: this.TEST_TIME + 60000 * 5,
+        whitelist_end_date: this.TEST_TIME + 60000 * 10,
+        sales_start_date: this.TEST_TIME + 60000 * 15,
+        sales_end_date: this.TEST_TIME + 60000 * 20,
+        distribution_start_date: this.TEST_TIME + 60000 * 25
+      }
+
+
+
   },
 
   methods: {
